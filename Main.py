613a8a4dde574a461fd12f1f2f43c8895088f8d0
ZEP4D <bkropiewnicki23@gmail.com>
2025-05-10 16:21:39 +0200

    0.0.3 PREALFA
    -Dodanie Zegar i obsługa go

--- Main.py
+++ Main.py
@@ -1,25 +1,78 @@
-import pygame
+from importlib.util import source_hash
 
+import pygame
+import pygame_widgets
+from pygame_widgets.button import Button
+from pygame_widgets.toggle import Toggle
+
+#core
 pygame.init()
 screen = pygame.display.set_mode((1280,720))
 clock = pygame.time.Clock()
 running = True
+font1 = pygame.font.Font('Font/IBMPlexMono-Regular.ttf',32)
+
+#--------------------------
+#Zegar
+minutes = 0
+hours = 7
+time_speed = 10
+time_passed = 0
+changeTime = 0
+
+def Zegar(dt):
+    global minutes,hours,time_passed
+
+    time_passed = time_passed + dt
+    if time_passed >= 1:
+        minutes= minutes + time_speed
+        time_passed = 0
+        
+        if minutes >=60:
+            hours += minutes // 60
+            minutes %= 60
+        if hours >= 24:
+            hours %= 24
+
+
+def Wyswielanie():
+    czas=f"{hours:02}:{minutes:02}"
+    showTime = font1.render(czas,True,"red")
+    showTimeRect= showTime.get_rect()
+    showTimeRect.x = 150
+    showTimeRect.y = 20
+    screen.blit(showTime,showTimeRect)
+
+def Zmiennczas(t):
+    global time_speed
+    time_speed= t
+
+
+StopButton = Button(screen,130,60,30,20, text='||', onClick= lambda: Zmiennczas(0))
+Speed1Button = Button(screen,160,60,30,20, text='>', onClick=lambda:Zmiennczas(10))
+Speed2Button = Button(screen,190,60,30,20, text='>>',onClick=lambda:Zmiennczas(30))
+Speed3Button = Button(screen,220,60,30,20, text='>>',onClick=lambda:Zmiennczas(50))
+#---------------
+
 
 while running:
     screen.fill("black")
-
+    dt = clock.tick(60) / 1000
+    Zegar(dt)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if pygame.mouse.get_pressed()[0]:
-                print("hello")
+                print("Yes")
 
     #lewa Strona Modul operacji
     pygame.draw.rect(screen,"purple",(0,0,400,720))
-
+    
     #Prawa Strona Modul mapy
     pygame.draw.rect(screen, "black", (401, 0, 880, 720))
+
+    Wyswielanie()
 
     x = 410
     for row in range(8):
@@ -32,7 +85,8 @@ while running:
 
     pygame.draw.line(screen, "black", (400, 0), (400, 720), width=3)
 
+    pygame_widgets.update(pygame.event.get())
     pygame.display.update()
-    clock.tick(60)
+
 
 pygame.quit()
